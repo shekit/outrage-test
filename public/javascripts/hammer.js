cb45e77bb60101844b25b905222b373eaa5c7810
shekit <as7790@nyu.edu>
2015-11-05 20:24:25 -0500

    added dummy panel to fix striker animation

--- public/javascripts/hammer.js
+++ public/javascripts/hammer.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
 	var count = -1;
 	var animateFunction;
-	var colors = ["#fbb040","#f7941e","#f16b2b","#ed1c24"];
+	var colors = ["red","#fbb040","#f7941e","#f16b2b","#ed1c24"];
 
 
 	setTimeout(function(){
 		$(".img-center").fadeOut(400);
 		$(".high-striker").fadeIn(800);
-		$(".panel0, .panel1, .panel2, .panel3").fadeIn(800);
+		$(".panel1, .panel2, .panel3, .panel4").fadeIn(800);
 		setTimeout(function(){
 			animate();
 		},900)
@@ -17,21 +17,24 @@ $(document).ready(function(){
 	function animate(){
 		animateFunction = setInterval(function(){
 		++count;
-		if(count>3){
+		if(count>4){
 			count = 0;
 			resetColors();
 		}
 		$(".panel"+count).css({'background':colors[count]})
-
-		console.log(count)
 		},400)
 	}
 	
 
 	function resetColors(){
-		for(var i=0;i<=3;i++){
+		for(var i=0;i<=4;i++){
 			$(".panel"+i).css({'background':'#ffffff'})
 		}
+	}
+
+	function reset(){
+		resetColors();
+		clearInterval(animateFunction);
 	}
 
 })
