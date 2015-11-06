a21075cd703eb9e7ef6991be79db45ea2efc8cad
shekit <as7790@nyu.edu>
2015-11-05 19:41:54 -0500

    added images, styling

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/hammer', function(req, res, next){
+	res.render('hammer')
+})
+
+router.get('match', function(req,res,next){
+	res.render('match')
+})
+
+router.get('control', function(req, res,next){
+	res.render('control')
+})
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
