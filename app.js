2caac5754064f71cfd55cfc8c98ec1a9a097814d
shekit <as7790@nyu.edu>
2015-11-18 10:00:08 -0500

    added twitter filters

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ var client = new TwitterStreamChannels(credentials)
 
 var channels = {
 	"bieber": ['bieber'],
-	"kim":["trump","donald trump"],
+	"kim":["trump","donald trump","kardashian","kim","miley"],
 	"aj":["bieber"],
 	"reb":["keystone"],
 	"abhi":["trump"]
